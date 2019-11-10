920a597d0ed1cfe10f41071f7000cf84d8b762f7
Sterling Walsh <sterling.walsh.dev@gmail.com>
2019-11-10 16:46:53 -0500

    add and style basic header component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import CssBaseline from '@material-ui/core/CssBaseline';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 // pages
-import About from './pages/About';
-import Solutions from './pages/Solutions';
-import FourZeroFour from './pages/FourZeroFour';
+import About from "./pages/About";
+import Solutions from "./pages/Solutions";
+import FourZeroFour from "./pages/FourZeroFour";
+
+import Nav from "./components/navigation/navigation.component";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -14,7 +16,7 @@ const useStyles = makeStyles(theme => ({
     height: "100vh",
     width: "100vw"
   }
-}))
+}));
 
 function App() {
   const classes = useStyles();
@@ -23,10 +25,11 @@ function App() {
       <CssBaseline />
       <main className={classes.root}>
         <Router>
+          <Nav />
           <Switch>
-            <Route exact path='/about' component={About} />
-            <Route exact path='/solutions' component={Solutions} />
-            <Route path='*' component={FourZeroFour} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/solutions" component={Solutions} />
+            <Route path="*" component={FourZeroFour} />
           </Switch>
         </Router>
       </main>
--- src/components/navigation/navigation.styles.js
+++ src/components/navigation/navigation.styles.js
@@ -1,37 +1,78 @@
+import styled, { css } from "styled-components";
 
-import styled, { css } from "styled-components"
+import { Link } from "react-router-dom";
 
 const floatStyles = css`
   box-shadow: 0 1px 5px 1px rgba(0, 0, 0, 0.25);
-`
+`;
 
-const getFloatStyles = ({ float }) => (float ? floatStyles : null)
+const getFloatStyles = ({ float }) => (float ? floatStyles : null);
 
 export const NavigationContainer = styled.nav`
   position: fixed;
+  top: 0;
   width: 100vw;
   z-index: 1000;
-  background: #fff;
+  background: ${({ theme }) => theme.palette.background.paper};
   height: 60px;
-  padding: 5px 50px;
+  padding: 0 2rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
   ${getFloatStyles};
-`
-NavigationContainer.displayName = "NavigationContainer"
+`;
+NavigationContainer.displayName = "NavigationContainer";
 
+// export const NavItemContainer = styled.div`
+//   display: flex;
 
-export const NavContentContainer = styled.div`
-  display: flex;
-
-  margin: 0 auto;
-  align-items: center;
-  justify-content: space-between;
-`
-NavContentContainer.displayName = "NavContentContainer"
-
+//   margin: 0 auto;
+//   align-items: center;
+//   justify-content: space-between;
+// `;
+// NavContentContainer.displayName = "NavContentContainer";
 
 export const NavLogo = styled.h1`
-    color: #af1313;
-    font-family: cursive;
-`
-NavLogo.displayName = "NavLogo"
+  ${"" /* color: #af1313; */}
+  font-family: "Kaushan Script";
+  margin-right: auto;
+`;
+NavLogo.displayName = "NavLogo";
+
+export const NavItem = styled(Link)`
+  border-bottom: ${props =>
+    props.current ? `2px solid ${props.theme.palette.text.primary}` : "none"};
+  margin: 0 1rem;
+  font-size: 18px;
+  font-weight: 500;
+  color: ${({ theme }) => theme.palette.text.primary};
+  text-decoration: none;
+  transition: border 0.25s ease-in-out;
+  box-sizing: border-box;
+
+  &:hover {
+    ${props =>
+      !props.current
+        ? `border-bottom:1px solid ${convertHex3To6(
+            props.theme.palette.text.primary
+          )}66`
+        : "none"};
+  }
+`;
+
+export const NavUserContainer = styled.div`
+  margin-left: auto;
+  display: flex;
+  align-items: center;
+`;
+
+export const NavUserName = styled.div``;
+
+export const NavUserIcon = styled.img``;
+
+const convertHex3To6 = hex =>
+  hex
+    .split("")
+    .reduce((acc, item, index) => acc + (index > 0 ? item : "") + item, "");
