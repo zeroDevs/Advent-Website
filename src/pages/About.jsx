c7120573cb7eb80774b872ae5f1f6df1866dc483
Abdus <azad3652@gmail.com>
2019-11-10 21:11:05 +0530

    FEATURE: Added pages: About, Solutions, 404.
    need to work on: bug with navbar. It hides page content.  could help

---
+++ src/pages/About.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function About(props) {
+  return (
+    <>
+      <h1>About Page</h1>
+    </>
+  );
+}
---
+++ src/pages/Solutions.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Solutions(props) {
+  return (
+    <>
+      <h1>Solutions Page</h1>
+    </>
+  );
+}
