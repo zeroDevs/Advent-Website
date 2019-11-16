80b9a2ea72df168085143285de8ed2b7595eb9f0
Matt Smith <mrmcchow@gmail.com>
2019-11-16 17:09:39 +0000

    Test login

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 
 export default function About(props) {
+  fetch(" https://aocbot.zerobot.xyz/api/login ")
+  .then(res => res.json())
+  .then(data => console.log(data));
   return (
     <>
       <div>About Page</div>
