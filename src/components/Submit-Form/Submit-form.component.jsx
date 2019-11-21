e32103a5d6b56d7ff99399db53d61f1c0dae95fd
Matt Smith <mrmcchow@gmail.com>
2019-11-21 03:13:42 +0000

    Bug fix

--- src/components/Submit-Form/Submit-form.component.jsx
+++ src/components/Submit-Form/Submit-form.component.jsx
@@ -10,136 +10,136 @@ import makeStyles from "@material-ui/core/styles/makeStyles";
 const listOfLangs = require("../../configs/languages.json");
 
 const useStyles = makeStyles(theme => ({
-  input: {
-    margin: theme.spacing(2, 0)
-  },
-  statusMessage: {
-    color: theme.palette.error.main,
-    height: theme.spacing(8),
-    marginTop: theme.spacing(2),
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center"
-  }
+	input: {
+		margin: theme.spacing(2, 0)
+	},
+	statusMessage: {
+		color: theme.palette.error.main,
+		height: theme.spacing(8),
+		marginTop: theme.spacing(2),
+		display: "flex",
+		justifyContent: "center",
+		alignItems: "center"
+	}
 }));
 
 function SubmitForm({ userName, handleClose }) {
-  const [date, setDate] = useState("2019-12-01");
-  const [url, setUrl] = useState("");
-  const [langName, setLangName] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
+	const [date, setDate] = useState("2019-12-01");
+	const [url, setUrl] = useState("");
+	const [langName, setLangName] = useState("");
+	const [errorMessage, setErrorMessage] = useState("");
+	const [isLoading, setIsLoading] = useState(false);
 
-  const classes = useStyles();
+	const classes = useStyles();
 
-  const handleSubmit = async event => {
-    event.preventDefault();
-    setIsLoading(true);
-    setErrorMessage("");
+	const handleSubmit = async event => {
+		event.preventDefault();
+		setIsLoading(true);
+		setErrorMessage("");
 
-    if (!validateURL(url)) {
-      return setErrorMessage(
-        "Invalid URL. Please use GitHub or GitHub Gist or repl.it to submit code."
-      );
-    }
+		if (!validateURL(url)) {
+			return setErrorMessage(
+				"Invalid URL. Please use GitHub or GitHub Gist or repl.it to submit code."
+			);
+		}
 
-    let body;
-    try {
-      body = JSON.stringify({ userName, url, date, langName }); // TODO: update property names to match expected body on backend
-    } catch (error) {
-      setErrorMessage("Invalid input");
-      return setIsLoading(false);
-    }
+		let body;
+		try {
+			body = JSON.stringify({ userName, url, date, langName }); // TODO: update property names to match expected body on backend
+		} catch (error) {
+			setErrorMessage("Invalid input");
+			return setIsLoading(false);
+		}
 
-    const submissionEndpoint = "/submit";
-    const { isSuccessful, error } = await fetch(submissionEndpoint, {
-      method: "POST",
-      headers: { "Content-Type": "application/json", Authorization: "Bearer " }, // TODO: add JWT here
-      body
-    });
+		const submissionEndpoint = "/submit";
+		const { isSuccessful, error } = await fetch(submissionEndpoint, {
+			method: "POST",
+			headers: { "Content-Type": "application/json", Authorization: "Bearer " }, // TODO: add JWT here
+			body
+		});
 
-    if (isSuccessful) {
-      handleClose();
-    } else {
-      setErrorMessage(error);
-      setIsLoading(false);
-    }
-  };
+		if (isSuccessful) {
+			handleClose();
+		} else {
+			setErrorMessage(error);
+			setIsLoading(false);
+		}
+	};
 
-  const handleUrlInputChange = event => setUrl(event.target.value);
-  const handleLangNameInputChange = event => setLangName(event.target.value);
-  const handleDateChange = event => setDate(event.target.value);
+	const handleUrlInputChange = event => setUrl(event.target.value);
+	const handleLangNameInputChange = event => setLangName(event.target.value);
+	const handleDateChange = event => setDate(event.target.value);
 
-  return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <div className={classes.statusMessage}>{errorMessage}</div>
+	return (
+		<>
+			<form onSubmit={handleSubmit}>
+				<div className={classes.statusMessage}>{errorMessage}</div>
 
-        <TextField
-          className={classes.input}
-          label="Username"
-          type="text"
-          value={userName}
-          disabled
-          fullWidth
-        />
+				<TextField
+					className={classes.input}
+					label="Username"
+					type="text"
+					value={userName}
+					disabled
+					fullWidth
+				/>
 
-        <TextField
-          className={classes.input}
-          label="Advent Challenge Date"
-          type="date"
-          value={date}
-          onChange={handleDateChange}
-          className={classes.textField}
-          InputLabelProps={{
-            shrink: true
-          }}
-          fullWidth
-        />
+				<TextField
+					className={classes.input}
+					label="Advent Challenge Date"
+					type="date"
+					value={date}
+					onChange={handleDateChange}
+					className={classes.textField}
+					InputLabelProps={{
+						shrink: true
+					}}
+					fullWidth
+				/>
 
-        <TextField
-          className={classes.input}
-          label="Solution Url"
-          type="text"
-          value={url}
-          onChange={handleUrlInputChange}
-          fullWidth
-        />
+				<TextField
+					className={classes.input}
+					label="Solution Url"
+					type="text"
+					value={url}
+					onChange={handleUrlInputChange}
+					fullWidth
+				/>
 
-        <Select
-          native
-          label="Language"
-          value={langName}
-          className={classes.input}
-          onChange={handleLangNameInputChange}
-          fullWidth
-        >
-          {listOfLangs.map(lang => (
-            <option key={lang} value={lang}>
-              {lang}
-            </option>
-          ))}
-        </Select>
-      </form>
-      <Button
-        variant="contained"
-        color="secondary"
-        onClick={handleSubmit}
-        fullWidth
-      >
-        {isLoading ? (
-          <CircularProgress color="secondary" size={25} />
-        ) : (
-          "Submit"
-        )}
-      </Button>
-    </>
-  );
+				<Select
+					native
+					label="Language"
+					value={langName}
+					className={classes.input}
+					onChange={handleLangNameInputChange}
+					fullWidth
+				>
+					{Object.keys(listOfLangs).map(lang => (
+						<option key={lang} value={lang}>
+							{lang}
+						</option>
+					))}
+				</Select>
+			</form>
+			<Button
+				variant="contained"
+				color="secondary"
+				onClick={handleSubmit}
+				fullWidth
+			>
+				{isLoading ? (
+					<CircularProgress color="secondary" size={25} />
+				) : (
+					"Submit"
+				)}
+			</Button>
+		</>
+	);
 }
 
 SubmitForm.propTypes = {
-  userName: PropTypes.string.isRequired,
-  handleClose: PropTypes.func.isRequired
+	userName: PropTypes.string.isRequired,
+	handleClose: PropTypes.func.isRequired
 };
 
 export default SubmitForm;
@@ -150,41 +150,41 @@ export default SubmitForm;
  */
 
 function validateURL(url = "") {
-  if (
-    !url.match(
-      /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/
-    )
-  ) {
-    return false;
-  }
+	if (
+		!url.match(
+			/^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/
+		)
+	) {
+		return false;
+	}
 
-  const { hostname, pathname } = new URL(url);
-  let flag = false;
+	const { hostname, pathname } = new URL(url);
+	let flag = false;
 
-  // eslint-disable-next-line
-  switch (hostname) {
-    case "www.github.com":
-      flag = true;
-      break;
+	// eslint-disable-next-line
+	switch (hostname) {
+		case "www.github.com":
+			flag = true;
+			break;
 
-    case "github.com":
-      flag = true;
-      break;
+		case "github.com":
+			flag = true;
+			break;
 
-    case "gist.github.com":
-      flag = true;
-      break;
+		case "gist.github.com":
+			flag = true;
+			break;
 
-    case "repl.it":
-      flag = true;
-      break;
+		case "repl.it":
+			flag = true;
+			break;
 
-    case "www.repl.it":
-      flag = true;
-      break;
-  }
+		case "www.repl.it":
+			flag = true;
+			break;
+	}
 
-  if (!pathname || pathname === "/") flag = false;
+	if (!pathname || pathname === "/") flag = false;
 
-  return flag;
+	return flag;
 }
