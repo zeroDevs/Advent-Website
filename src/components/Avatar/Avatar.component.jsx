b8c01f4153b436ec56fa3957222361bcffeca197
Sterling Walsh <sterling.walsh.dev@gmail.com>
2019-11-23 16:48:23 -0500

    submit modal to page, submit layout, submit route, some other tweaks

--- src/components/Avatar/Avatar.component.jsx
+++ src/components/Avatar/Avatar.component.jsx
@@ -1,37 +1,39 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import MaterialAvatar from '@material-ui/core/Avatar';
+import React from "react";
+import PropTypes from "prop-types";
+import MaterialAvatar from "@material-ui/core/Avatar";
 
-import makeStyles from '@material-ui/core/styles/makeStyles';
+import makeStyles from "@material-ui/core/styles/makeStyles";
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    width: theme.spacing(20),
-    height: theme.spacing(20),
-    margin: [[0, 'auto']],
-  },
+	root: {
+		width: theme.spacing(20),
+		height: theme.spacing(20),
+		margin: [[0, "auto"]],
+		background: "rgba(0,0,0,0)"
+	}
 }));
 
 function Avatar({ id, avatar, userName, ...props }) {
-  const classes = useStyles({ classes: props.classes });
-  const discordImgUrl =
-    id && avatar
-      ? `https://cdn.discordapp.com/avatars/${id}/${avatar}.png?size=512`
-      : 'http://www.gravatar.com/avatar/b028e9ca948dbc90143f598ad3c254b6?s=200&r=pg&d=mm';
+	const classes = useStyles({ classes: props.classes });
+	const discordImgUrl =
+		id && avatar
+			? `https://cdn.discordapp.com/avatars/${id}/${avatar}.png?size=512`
+			: "images/user-unknown.png";
 
-  return (
-    <MaterialAvatar
-      alt={userName}
-      src={discordImgUrl}
-      className={classes.root}
-    />
-  );
+	return (
+		<MaterialAvatar
+			variant={`${id && avatar ? "circle" : "square"}`}
+			alt={userName}
+			src={discordImgUrl}
+			className={classes.root}
+		/>
+	);
 }
 
 Avatar.propTypes = {
-  id: PropTypes.string,
-  avatar: PropTypes.string,
-  userName: PropTypes.string,
+	id: PropTypes.string,
+	avatar: PropTypes.string,
+	userName: PropTypes.string
 };
 
 export default Avatar;
