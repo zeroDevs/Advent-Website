954712eda99c5587153768960c66154a35f07268
Brittney Postma <brittney.postma@hotmail.com>
2019-11-24 19:46:22 -0500

    removed picture element and just served pngs

--- src/components/HeroSection/HeroSection.component.jsx
+++ src/components/HeroSection/HeroSection.component.jsx
@@ -1,21 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
-// import { useTheme } from "@material-ui/core/styles";
-// import useMediaQuery from "@material-ui/core/useMediaQuery";
+import { useTheme } from "@material-ui/core/styles";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 import makeStyles from "@material-ui/core/styles/makeStyles";
 
 const useStyles = makeStyles(theme => ({
-	root: () => { 
-		if (window.matchMedia('(max-width: 600px)').matches) {
+	root: props => { 
+		if (props.isMobile) {
 			return {
 				backgroundPosition: "center center",
 				height: "100%"
 			};
 		} else {
 			return {
-				height: "100%",
-				display: "grid",
-				gridTemplateColumns: "33% 1fr"
+				backgroundPosition: 'center center',
+				height: "100%"
 			};
 		}
 	},
@@ -36,10 +35,9 @@ const useStyles = makeStyles(theme => ({
 }));
 
 function HeroSection({ children, ...props }) {
-	// const theme = useTheme();
-	// const isMobile = useMediaQuery(theme.breakpoints.down("xs"));
-	// console.log(isMobile)
-	const classes = useStyles({ classes: props.classes });
+	const theme = useTheme();
+	const isMobile = useMediaQuery(theme.breakpoints.down("xs"));
+	const classes = useStyles({ isMobile, classes: props.classes });
 	const getUrl = () => 
 		window.location.href === 'https://aoc.zerotomastery.io/404' || 
 		window.location.href === 'http://localhost:3000/404' ? 
@@ -48,33 +46,20 @@ function HeroSection({ children, ...props }) {
 	return (
 		<div className={classes.root}>
 			{getUrl() ? (
-			<picture>
-				<source srcset="images/adventlightsout.png" media="(min-width: 601px)" />
-				<source srcset="images/adventlightsout-mobile.png" media="(max-width: 600px)" />
-				<img
+		
+			<img
 					src="images/adventlightsout.png"
 					alt="Ascii Christmas Tree"
 					className={classes.bg}
 					loading="lazy"
 				/>
-			</picture>
 			) : (
-				<picture>
-				<source
-					srcset="images/advent.webp"
-					type="image/webp"
-					media="(min-width: 601px)"
-				/>
-				<source srcset="images/advent.png" media="(min-width: 601px)" />
-				<source srcset="images/advent-mobile.webp" media="(max-width: 600px)" />
-				<source srcset="images/advent-mobile.png" media="(max-width: 600px)" />
-				<img
+			<img
 					src="images/advent.png"
 					alt="Ascii Christmas Tree"
 					className={classes.bg}
 					loading="lazy"
 				/>
-			</picture>
 			)}
 			
 
