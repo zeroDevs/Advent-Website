ae568d79fceeca4e9dce7eab15d615a166b30049
Sterling Walsh <sterling.walsh.dev@gmail.com>
2019-11-26 17:19:25 -0500

    tweaks

--- src/components/Submit-Form/Submit-form.component.jsx
+++ src/components/Submit-Form/Submit-form.component.jsx
@@ -154,96 +154,90 @@ function SubmitForm({ user }) {
 
 	return (
 		<>
-			
-				<div className={classes.infoContainer}>
-					{!user ? (
-						<Button
-							variant="contained"
-							color="secondary"
-							to={{
-								pathname: "/login",
-								state: { from: "/submit" }
-							}}
-							component={Link}
-						>
-							Please Login
-						</Button>
-						
-					) : (
-						<>
-						<form onSubmit={handleSubmit}>
-							<div className={classes.name}>{userName}</div>
-							{message ? (
-								<div className={`${isError ? classes.error : classes.success}`}>
-									{message.split("\n").reduce((acc, item, index, arr) => {
-										acc.push(item);
-										if (index !== arr.length - 1) acc.push(<br></br>);
-										return acc;
-									}, [])}
-								</div>
-							) : null}
-							<div className={classes.row}>
-								<MuiPickersUtilsProvider utils={DateFnsUtils}>
-									<DatePicker
-										format="MM/dd/yyyy"
-										id="date-picker-inline"
-										label="Advent Challenge Date"
-										value={date}
-										onChange={handleDateChange}
-										className={classes.input}
-									/>
-								</MuiPickersUtilsProvider>
-						
-								<FormControl className={classes.input}>
-									<InputLabel id="demo-simple-select-placeholder-label-label">
-										Language
-									</InputLabel>
-									<Select
-										labelId="demo-simple-select-placeholder-label-label"
-										id="demo-simple-select-placeholder-label"
-										value={langName}
-										onChange={handleLangNameInputChange}
-									>
-										{Object.keys(listOfLangs).map(lang => (
-											<MenuItem key={lang} value={lang}>
-												{lang}
-											</MenuItem>
-										))}
-									</Select>
-								</FormControl>
-					
-				
-								<TextField
-									className={classes.input}
-									label="Solution Url"
-									type="text"
-									value={url}
-									onChange={handleUrlInputChange}
-									fullWidth
-								/>
-								</div>
-						</form>
-						<Button
-							variant="contained"
-							color="secondary"
-							onClick={handleSubmit}
-							fullWidth
-							disabled={!user}
-							className={classes.input}
-						>
-							{isLoading ? (
-								<CircularProgress color="secondary" size={25} />
-							) : (
-								"Submit"
-							)}
-						</Button>
-			
-						</>
-					)}
-				
-				</div>
-			
-		
+			<div className={classes.infoContainer}>
+				{user ? (
+					<>
+						<div className={classes.name}>{userName}</div>
+						{message ? (
+							<div className={`${isError ? classes.error : classes.success}`}>
+								{message.split("\n").reduce((acc, item, index, arr) => {
+									acc.push(item);
+									if (index !== arr.length - 1) acc.push(<br></br>);
+									return acc;
+								}, [])}
+							</div>
+						) : null}
+					</>
+				) : (
+					<Button
+						variant="contained"
+						color="secondary"
+						to={{
+							pathname: "/login",
+							state: { from: "/submit" }
+						}}
+						component={Link}
+					>
+						Please Login
+					</Button>
+				)}
+			</div>
+			{user ? (
+				<form onSubmit={handleSubmit}>
+					<div className={classes.row}>
+						<MuiPickersUtilsProvider utils={DateFnsUtils}>
+							<DatePicker
+								format="MM/dd/yyyy"
+								id="date-picker-inline"
+								label="Advent Challenge Date"
+								value={date}
+								onChange={handleDateChange}
+								className={classes.input}
+							/>
+						</MuiPickersUtilsProvider>
+						<FormControl className={classes.input}>
+							<InputLabel id="demo-simple-select-placeholder-label-label">
+								Language
+							</InputLabel>
+							<Select
+								labelId="demo-simple-select-placeholder-label-label"
+								id="demo-simple-select-placeholder-label"
+								value={langName}
+								onChange={handleLangNameInputChange}
+							>
+								{Object.keys(listOfLangs).map(lang => (
+									<MenuItem key={lang} value={lang}>
+										{lang}
+									</MenuItem>
+								))}
+							</Select>
+						</FormControl>
+					</div>
+					<TextField
+						className={classes.input}
+						label="Solution Url"
+						type="text"
+						value={url}
+						onChange={handleUrlInputChange}
+						fullWidth
+					/>
+
+					<Button
+						variant="contained"
+						color="secondary"
+						onClick={handleSubmit}
+						fullWidth
+						disabled={!user}
+						className={classes.input}
+					>
+						{isLoading ? (
+							<CircularProgress color="secondary" size={25} />
+						) : (
+							"Submit"
+						)}
+					</Button>
+				</form>
+			) : null}
 		</>
 	);
 }
