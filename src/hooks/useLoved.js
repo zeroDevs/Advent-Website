9c7ce1b3b8b09f661d1760940a7402d4edcaad1c
Meet1103 <47909913+Meet1103@users.noreply.github.com>
2019-11-29 00:50:30 -0700

    Update useLoved.js

--- src/hooks/useLoved.js
+++ src/hooks/useLoved.js
@@ -5,12 +5,8 @@ export default function useRecent() {
 
 	useEffect(() => {
 		async function callLovedApiEndpoint() {
-			let qty;
-			if(window.matchMedia(`(max-width:600px)`)) {
-				qty = 3;
-			} else {
-				qty = 6;
-			}
+			const width = window.innerWidth < 600;
+			const qty = width ? 3 : 6;
 			const response = await fetch(`https://aocbot.zerobot.xyz/solutions/top?qty=${qty}`);
 			const data = await response.json();
 			setLoved(data);
