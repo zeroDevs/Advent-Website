c8e3b7b542a7a146e5061d5890a74be5c48cefcd
Meet1103 <47909913+Meet1103@users.noreply.github.com>
2019-11-29 00:51:39 -0700

    Update useRecent.js

--- src/hooks/useRecent.js
+++ src/hooks/useRecent.js
@@ -7,7 +7,6 @@ export default function useRecent() {
 		async function callRecentApiEndpoint() {
 			const width = window.innerWidth < 600;
 			const qty = width ? 3 : 6;
-			window.addEventListener("resize", abc);
 			const response = await fetch(`https://aocbot.zerobot.xyz/solutions/recent?qty=${qty}`);
 			const data = await response.json();
 			setRecent(data);
