cf35ff972079fe4a0bed79461983c5b25f484f36
Matt Smith <mrmcchow@gmail.com>
2019-11-30 02:26:59 +0000

    Misc tweaks
    1 - Updated tree image
    2 - Improved carousel sliding
    3 - Attempted to make a more uniformed collab gird
    4. Tweaked spacing in highlight section

--- src/components/Contributors/Contributors.component.jsx
+++ src/components/Contributors/Contributors.component.jsx
@@ -6,39 +6,55 @@ import ContribCard from "./ContribCard.component";
 import data from "./contributors.json";
 
 const useStyles = makeStyles(theme => ({
-  contributorsContainer: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(4, 0)
-  },
-  cardContainer: {
-    display: "flex",
-    flexWrap: "wrap",
-    justifyContent: "center"
-  }
+	contributorsContainer: {
+		backgroundColor: theme.palette.background.paper,
+		padding: theme.spacing(4, 0)
+	},
+	cardContainer: {
+		display: "flex",
+		flexWrap: "wrap",
+		justifyContent: "center",
+		margin: theme.spacing(4, 0),
+		marginLeft: "auto",
+		marginRight: "auto",
+		width: "95%",
+		[theme.breakpoints.up("sm")]: {
+			width: "80%"
+		},
+		[theme.breakpoints.up("md")]: {
+			width: "95%"
+		},
+		[theme.breakpoints.up("lg")]: {
+			width: "90%"
+		},
+		[theme.breakpoints.up("xl")]: {
+			width: "70%"
+		}
+	}
 }));
 
 function Contributors(props) {
-  const classes = useStyles();
+	const classes = useStyles();
 
-  return (
-    <>
-      <div className={classes.contributorsContainer}>
-        <Typography
-          align="center"
-          color="textPrimary"
-          variant="h3"
-          gutterBottom
-        >
-          Contributors
-        </Typography>
-        <div className={classes.cardContainer}>
-          {data.map(person => (
-            <ContribCard key={person.name} person={person} />
-          ))}
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className={classes.contributorsContainer}>
+				<Typography
+					align="center"
+					color="textPrimary"
+					variant="h3"
+					gutterBottom
+				>
+					Contributors
+				</Typography>
+				<div className={classes.cardContainer}>
+					{data.map(person => (
+						<ContribCard key={person.name} person={person} />
+					))}
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default React.memo(Contributors);
