6e29509fabf8de6f23fdb87106c66456a8e29ec3
Matt Smith <mrmcchow@gmail.com>
2021-11-27 22:06:25 +0000

    Update endpoints

--- src/hooks/useLoved.js
+++ src/hooks/useLoved.js
@@ -7,7 +7,9 @@ export default function useRecent() {
 		async function callLovedApiEndpoint() {
 			const width = window.innerWidth < 600;
 			const qty = width ? 3 : 6;
-			const response = await fetch(`https://aocbot.zerobot.xyz/solutions/top?qty=${qty}`);
+			const response = await fetch(
+				`https://aocbot.zerobot.app/solutions/top?qty=${qty}`
+			);
 			const data = await response.json();
 			setLoved(data);
 		}
